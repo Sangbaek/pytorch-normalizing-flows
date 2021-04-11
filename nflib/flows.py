63aacef3077d5e0e6520cdaa5bedd8d1c3807d1b
Sangbaek <sangbaek@mit.edu>
2021-04-11 19:44:20 -0400

    save only NICE part

--- nflib/flows.py
+++ nflib/flows.py
@@ -9,25 +9,6 @@ Reference:
 NICE: Non-linear Independent Components Estimation, Dinh et al. 2014
 https://arxiv.org/abs/1410.8516
 
-Variational Inference with Normalizing Flows, Rezende and Mohamed 2015
-https://arxiv.org/abs/1505.05770
-
-Density estimation using Real NVP, Dinh et al. May 2016
-https://arxiv.org/abs/1605.08803
-(Laurent's extension of NICE)
-
-Improved Variational Inference with Inverse Autoregressive Flow, Kingma et al June 2016
-https://arxiv.org/abs/1606.04934
-(IAF)
-
-Masked Autoregressive Flow for Density Estimation, Papamakarios et al. May 2017 
-https://arxiv.org/abs/1705.07057
-"The advantage of Real NVP compared to MAF and IAF is that it can both generate data and estimate densities with one forward pass only, whereas MAF would need D passes to generate data and IAF would need D passes to estimate densities."
-(MAF)
-
-Glow: Generative Flow with Invertible 1x1 Convolutions, Kingma and Dhariwal, Jul 2018
-https://arxiv.org/abs/1807.03039
-
 "Normalizing Flows for Probabilistic Modeling and Inference"
 https://arxiv.org/abs/1912.02762
 (review paper)
@@ -35,10 +16,7 @@ https://arxiv.org/abs/1912.02762
 
 import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn
-
-from nflib.nets import LeafParam, MLP, ARMLP
 
 class AffineConstantFlow(nn.Module):
     """ 
@@ -63,27 +41,6 @@ class AffineConstantFlow(nn.Module):
         x = (z - t) * torch.exp(-s)
         log_det = torch.sum(-s, dim=1)
         return x, log_det
-
-
-class ActNorm(AffineConstantFlow):
-    """
-    Really an AffineConstantFlow but with a data-dependent initialization,
-    where on the very first batch we clever initialize the s,t so that the output
-    is unit gaussian. As described in Glow paper.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.data_dep_init_done = False
-    
-    def forward(self, x):
-        # first batch is used for init
-        if not self.data_dep_init_done:
-            assert self.s is not None and self.t is not None # for now
-            self.s.data = (-torch.log(x.std(dim=0, keepdim=True))).detach()
-            self.t.data = (-(x * torch.exp(self.s)).mean(dim=0, keepdim=True)).detach()
-            self.data_dep_init_done = True
-        return super().forward(x)
-
 
 class AffineHalfFlow(nn.Module):
     """
@@ -132,115 +89,22 @@ class AffineHalfFlow(nn.Module):
         log_det = torch.sum(-s, dim=1)
         return x, log_det
 
+class MLP(nn.Module):
+    """ a simple 4-layer MLP (Multilayer perceptron)"""
 
-class SlowMAF(nn.Module):
-    """ 
-    Masked Autoregressive Flow, slow version with explicit networks per dim
-    """
-    def __init__(self, dim, parity, net_class=MLP, nh=24):
+    def __init__(self, nin, nout, nh):
         super().__init__()
-        self.dim = dim
-        self.layers = nn.ModuleDict()
-        self.layers[str(0)] = LeafParam(2)
-        for i in range(1, dim):
-            self.layers[str(i)] = net_class(i, 2, nh)
-        self.order = list(range(dim)) if parity else list(range(dim))[::-1]
-        
+        self.net = nn.Sequential(
+            nn.Linear(nin, nh),
+            nn.LeakyReLU(0.2),
+            nn.Linear(nh, nh),
+            nn.LeakyReLU(0.2),
+            nn.Linear(nh, nh),
+            nn.LeakyReLU(0.2),
+            nn.Linear(nh, nout),
+        )
     def forward(self, x):
-        z = torch.zeros_like(x)
-        log_det = torch.zeros(x.size(0))
-        for i in range(self.dim):
-            st = self.layers[str(i)](x[:, :i])
-            s, t = st[:, 0], st[:, 1]
-            z[:, self.order[i]] = x[:, i] * torch.exp(s) + t
-            log_det += s
-        return z, log_det
-
-    def backward(self, z):
-        x = torch.zeros_like(z)
-        log_det = torch.zeros(z.size(0))
-        for i in range(self.dim):
-            st = self.layers[str(i)](x[:, :i])
-            s, t = st[:, 0], st[:, 1]
-            x[:, i] = (z[:, self.order[i]] - t) * torch.exp(-s)
-            log_det += -s
-        return x, log_det
-
-class MAF(nn.Module):
-    """ Masked Autoregressive Flow that uses a MADE-style network for fast forward """
-    
-    def __init__(self, dim, parity, net_class=ARMLP, nh=24):
-        super().__init__()
-        self.dim = dim
-        self.net = net_class(dim, dim*2, nh)
-        self.parity = parity
-
-    def forward(self, x):
-        # here we see that we are evaluating all of z in parallel, so density estimation will be fast
-        st = self.net(x)
-        s, t = st.split(self.dim, dim=1)
-        z = x * torch.exp(s) + t
-        # reverse order, so if we stack MAFs correct things happen
-        z = z.flip(dims=(1,)) if self.parity else z
-        log_det = torch.sum(s, dim=1)
-        return z, log_det
-    
-    def backward(self, z):
-        # we have to decode the x one at a time, sequentially
-        x = torch.zeros_like(z)
-        log_det = torch.zeros(z.size(0))
-        z = z.flip(dims=(1,)) if self.parity else z
-        for i in range(self.dim):
-            st = self.net(x.clone()) # clone to avoid in-place op errors if using IAF
-            s, t = st.split(self.dim, dim=1)
-            x[:, i] = (z[:, i] - t[:, i]) * torch.exp(-s[:, i])
-            log_det += -s[:, i]
-        return x, log_det
-
-class IAF(MAF):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        """
-        reverse the flow, giving an Inverse Autoregressive Flow (IAF) instead, 
-        where sampling will be fast but density estimation slow
-        """
-        self.forward, self.backward = self.backward, self.forward
-
-
-class Invertible1x1Conv(nn.Module):
-    """ 
-    As introduced in Glow paper.
-    """
-    
-    def __init__(self, dim):
-        super().__init__()
-        self.dim = dim
-        Q = torch.nn.init.orthogonal_(torch.randn(dim, dim))
-        P, L, U = torch.lu_unpack(*Q.lu())
-        self.P = P # remains fixed during optimization
-        self.L = nn.Parameter(L) # lower triangular portion
-        self.S = nn.Parameter(U.diag()) # "crop out" the diagonal to its own parameter
-        self.U = nn.Parameter(torch.triu(U, diagonal=1)) # "crop out" diagonal, stored in S
-
-    def _assemble_W(self):
-        """ assemble W from its pieces (P, L, U, S) """
-        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.dim))
-        U = torch.triu(self.U, diagonal=1)
-        W = self.P @ L @ (U + torch.diag(self.S))
-        return W
-
-    def forward(self, x):
-        W = self._assemble_W()
-        z = x @ W
-        log_det = torch.sum(torch.log(torch.abs(self.S)))
-        return z, log_det
-
-    def backward(self, z):
-        W = self._assemble_W()
-        W_inv = torch.inverse(W)
-        x = z @ W_inv
-        log_det = -torch.sum(torch.log(torch.abs(self.S)))
-        return x, log_det
+        return self.net(x)
 
 # ------------------------------------------------------------------------
 
